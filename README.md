1bed64d8fdd3164aab3384200714b6c8de59b1e8
Braxton Diggs <braxtondiggs@gmail.com>
2021-04-02 20:32:51 -0400

    Ver 2.0, Working Prototype

--- README.md
+++ README.md
@@ -2,17 +2,8 @@
 
 Get, convert and store all episodes of The All Out Show from Youtube
 
-## Getting Started
-
-### Prerequisites
-- [Node.js and npm](nodejs.org) Node ^4.2.3, npm ^2.14.7
-- [Heroku CLI](https://devcenter.heroku.com/articles/getting-started-with-nodejs) Heroku ^5.6.8
-- [Node Foreman](https://github.com/strongloop/node-foreman) (`npm install --global foreman`)
-
 ## Build & Development
 
 1. Run `npm install` to install server dependencies.
 
-2. Run `heroku config:get GOOGLE_API -s >> .env --app articuno1 && heroku config:get S3_KEY -s >> .env --app articuno1 && heroku config:get S3_SECRET -s >> .env --app articuno1 && heroku config:get MONGODB_URI -s >> .env --app articuno1` to install Heroku Environment Variables.
-
-3. Run `nf run npm run watch` to start the development server.
+2. Run `npm start` to start the server.
---
+++ src/index.ts
@@ -0,0 +1,65 @@
+const { launch, getStream } = require("puppeteer-stream");
+import * as fs from 'fs';
+import * as _ from 'lodash';
+
+
+(async () => {
+    try {
+        const browser = await launch({
+            executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+        });
+        const page = await browser.newPage();
+
+        await page.goto('https://player.siriusxm.com/login');
+
+        await page.type('#username', '');
+        await page.type('#password', '');
+
+        await page.click('.login-button');
+        await page.waitForNavigation({ waitUntil: 'networkidle2' });
+
+        await page.goto('https://player.siriusxm.com/favorites/shows');
+        await page.waitForNavigation({ waitUntil: 'networkidle2' });
+
+        await page.click('.tile-wrapper');
+        await page.waitForTimeout(250);
+
+        await page.click('.tile-list__item:nth-child(1) button.center-column');
+        await page.waitForTimeout(250);
+
+        await page.click('.play-pause-btn');
+
+        const title = await page.$eval('.info-container__title span', el => el.innerText);
+
+        const file = fs.createWriteStream( `${__dirname}/${title}.webm`);
+        const stream = await getStream(page, { audio: true, video: false, mimeType: 'audio/webm' });
+        stream.pipe(file);
+
+        console.log("recording");
+
+        await new Promise((resolve, _reject) => {
+            let progress = 0;
+            const progressbar = setInterval(async () => {
+                const width = await page.$eval('#current-listening-position', el => el.getAttribute('style'));
+                progress = _.chain(width).split('width: ', 2).last().replace('%;', '').toNumber().value();
+
+                if (progress === 100) {
+                    await stream.destroy();
+                    file.close();
+                    clearInterval(progressbar);
+                    return resolve('success');
+                }
+            }, 1000);
+        });
+
+        console.log("finished");
+
+        // Close the browser, we no longer need it
+        await browser.close();
+
+    } catch (e) {
+        console.log(e);
+    }
+
+
+})();
