3295a60e05a15797990a63b7bbd3e80e32a4b517
Braxton Diggs <braxtondiggs@gmail.com>
2021-04-04 23:05:44 -0400

    add firebase admin

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 const { launch, getStream } = require("puppeteer-stream");
 import * as fs from 'fs';
 import * as _ from 'lodash';
-
+import * as admin from "firebase-admin";
+var serviceAccount = require("../serviceAccountKey.json");
 
 (async () => {
     try {
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+            storageBucket: "bulbasaur-bfb64.appspot.com"
+        });
+
         const browser = await launch({
             executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
         });
@@ -20,19 +26,21 @@ import * as _ from 'lodash';
 
         await page.goto('https://player.siriusxm.com/favorites/shows');
         await page.waitForNavigation({ waitUntil: 'networkidle2' });
+        await page.waitForTimeout(500);
 
         await page.click('.tile-wrapper');
-        await page.waitForTimeout(250);
+        await page.waitForTimeout(500);
 
         await page.click('.tile-list__item:nth-child(1) button.center-column');
-        await page.waitForTimeout(250);
+        await page.waitForTimeout(500);
 
         await page.click('.play-pause-btn');
 
         const title = await page.$eval('.info-container__title span', el => el.innerText);
 
-        const file = fs.createWriteStream( `${__dirname}/${title}.webm`);
+        const file = fs.createWriteStream(`./outputs/${encodeURIComponent(title)}.webm`);
         const stream = await getStream(page, { audio: true, video: false, mimeType: 'audio/webm' });
+
         stream.pipe(file);
 
         console.log("recording");
@@ -50,6 +58,14 @@ import * as _ from 'lodash';
                     return resolve('success');
                 }
             }, 1000);
+        });
+
+        await admin.storage().bucket().upload(`./outputs/${encodeURIComponent(title)}.webm`, {
+            destination: `articuno/${encodeURIComponent(title)}.webm`, metadata: {
+                contentType: 'audio/webm'
+            },
+            public: true,
+            validation: "md5"
         });
 
         console.log("finished");
